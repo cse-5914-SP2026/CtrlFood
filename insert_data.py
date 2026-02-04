e69518e12ff902aa978608bf69bc56ead550c3ae
singh1773 <singh.1733@osu.edu>
2026-02-04 16:55:56 -0500

    Added to docker file and created script for getting and putting data into elastic

---
+++ insert_data.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+api_url = "https://osu.api.nutrislice.com/menu/api/weeks/school/12th-avenue-bread-company/menu-type/all-day/2026/02/03/"
+response = requests.get(api_url)
+
+if response.status_code != 200:
+    print(response.status_code, response.text)
+    exit(1)
+
+data = response.json()
+
+# Flatten menu items
+menu_items = []
+for day in data.get("days"):
+    date = day.get("date")
+    for item in day.get("menu_items"):
+        food_name = item["food"].get("name")
+        menu_items.append({
+            "name": food_name,
+            "date": date,
+            "menu_type_id": data.get("menu_type_id")
+        })
+
+# Build bulk payload
+bulk_data = ""
+for item in menu_items:
+    bulk_data += json.dumps({"index": {"_index": "foods"}}) + "\n"
+    bulk_data += json.dumps(item) + "\n"
+
+# Send to Elasticsearch
+es_url = "http://localhost:9200/_bulk"
+headers = {"Content-Type": "application/json"}
+res = requests.post(es_url, headers=headers, data=bulk_data.encode("utf-8"))
+
+print(res.text)
