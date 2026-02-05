4f6494bf77b8b22568c18f3b6b9862b99c650b9f
singh1773 <singh.1733@osu.edu>
2026-02-05 13:14:20 -0500

    Fixed local to docker

--- backend/elastic_search/insert_data.py
+++ backend/elastic_search/insert_data.py
@@ -32,7 +32,7 @@ for item in menu_items:
     bulk_data += json.dumps(item) + "\n"
 
 # Send to Elasticsearch
-es_url = "http://localhost:9200/_bulk"
+es_url = "http://elastic_search:9200/_bulk"
 headers = {"Content-Type": "application/json"}
 res = requests.post(es_url, headers=headers, data=bulk_data.encode("utf-8"))
 
