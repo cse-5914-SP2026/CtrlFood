1a6a3824e7e22fd8962f60ada739036e274336b4
Ryan <liu.10260@buckeyemail.osu.edu>
2026-02-12 15:38:28 -0500

    filter by date (backend) hardcoded addresses into JSON returned by backend

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -3,3 +3,5 @@ from pydantic import BaseModel
 # TODO this is for prototyping may need to change as app changes
 class UserQuery(BaseModel):
     query: str
+    date: str | None = None
+    location: str | None = None
--- backend/app/utility.py
+++ backend/app/utility.py
@@ -1,13 +1,31 @@
-def make_es_search(query: str):
-    return(
+def get_distance(loc1, loc2):
+    # TODO: calc distance between user loc and restaurant loc 
+    # and then tiebreak by distance if the score is the same
+    # by passing in the distance to the es query?
+    pass
+
+def make_es_search(query: str, date: str = None):
+    must = [
         {
-            "query": {
-                "match": {
-                    "name": {
-                        "query": f"{query}",
-                        "fuzziness": "AUTO"
-                    }
+            "match": {
+                "name": {
+                    "query": query,
+                    "fuzziness": "AUTO"
                 }
             }
         }
-    )
+    ]
+    # filter -> satisfy this but don't score it 
+    filter_clauses = []
+    if date:
+        # term: exact match, no fuzziness
+        filter_clauses.append({"term": {"date": date}})
+
+    return {
+        "query": {
+            "bool": {
+                "must": must,
+                "filter": filter_clauses
+            }
+        }
+    }
