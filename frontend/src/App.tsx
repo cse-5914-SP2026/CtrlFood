0f82cb20d81b65e64326700b1a3aeb27a307fd80
Austin Bian <iaustin0b@gmail.com>
2026-02-14 01:48:34 -0500

    Rearrange components and add wrappers for rerender perfomrance tweaks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -68,58 +68,18 @@
 import { SearchBox } from "./components/SearchBox";
 import * as React from "react";
 import { ThemeProvider } from "./components/theme-provider";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
 import 'leaflet/dist/leaflet.css';
-import MyMap from "./components/test-map";
-import { CategoryIslands } from "./components/category-islands";
-import { SearchBar } from "./components/SearchBar";
-import LeftBar from "./components/left-bar";
-import { DatePicker } from "./components/date-picker";
-import { Calendar } from "./components/ui/calendar";
-import QueryList from "./components/query-list";
-import { queryStore } from "./store/store";
+import OverlayWrapper from "./components/OverlayWrapper";
+import MapWrapper from "./components/MapWrapper";
 
 function App() {
-  const [foodResult, setFoodResult] = React.useState<string | null>(null);
-
-  const updateCurrentQueryList = queryStore((state) => state.populateBackendQueryList)
-
-  const handleSearch = async (q: string) => {
-    const res = await fetch("http://localhost:8000/query", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query: q }),
-    });
-
-    if (!res.ok) {
-      const text = await res.text();
-      console.error("API error:", text);
-      return;
-    }
-    const data = await res.json();
-    console.log("ES results:", data);
-    setFoodResult(
-      data ? ` Found: ${JSON.stringify(data)}` : " No results found.",
-    );
-
-    updateCurrentQueryList(data) // set new to global store
-  };
 
   return (
     <ThemeProvider>
       <div className="relative h-screen w-screen">
 
-        <MyMap></MyMap>
-        <CategoryIslands></CategoryIslands>
-        <SearchBar onSearch={handleSearch}></SearchBar>
-        <LeftBar></LeftBar>
-        <DatePicker></DatePicker>
-        <QueryList></QueryList>
-        {/* <Calendar
-          mode="single"
-          className="rounded-lg border"
-          captionLayout="dropdown"
-        /> */}
+        <MapWrapper></MapWrapper>
+        <OverlayWrapper></OverlayWrapper>
 
       </div>
     </ThemeProvider>
