fd894c1ff3149f6689a677900f2846872a157f9a
Austin Bian <iaustin0b@gmail.com>
2026-02-17 17:35:15 -0500

    Init custom merge from frontend/austin to lemeng branch

---
+++ frontend/src/components/OverlayWrapper.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { CategoryIslands } from './category-islands'
+import { DatePicker } from './date-picker'
+import LeftBar from './left-bar'
+import QueryList from './query-list'
+import { SearchBar } from './SearchBar'
+import { queryStore } from '@/store/store'
+
+const OverlayWrapper = () => {
+
+    const [foodResult, setFoodResult] = React.useState<string | null>(null);
+
+    const updateCurrentQueryList = queryStore((state) => state.populateBackendQueryList)
+
+    const handleSearch = async (q: string) => {
+        const res = await fetch("http://localhost:8000/query", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ query: q }),
+        });
+
+        if (!res.ok) {
+            const text = await res.text();
+            console.error("API error:", text);
+            return;
+        }
+        const data = await res.json();
+        console.log("ES results:", data);
+        setFoodResult(
+            data ? ` Found: ${JSON.stringify(data)}` : " No results found.",
+        );
+
+        updateCurrentQueryList(data) // set new to global store
+    };
+
+    return (
+        <>
+            <CategoryIslands></CategoryIslands>
+            <SearchBar onSearch={handleSearch}></SearchBar>
+            <LeftBar></LeftBar>
+            <DatePicker></DatePicker>
+            <QueryList></QueryList>
+        </>
+    )
+}
+
+export default OverlayWrapper
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,41 +1,64 @@
-import * as React from "react";
+// import * as React from "react";
+// import { SearchBox } from "../components/SearchBox";
+
+// export default function MainPage() {
+//   const [foodResult, setFoodResult] = React.useState<string | null>(null);
+
+//   const handleSearch = async (q: string) => {
+//     const res = await fetch("http://localhost:8000/query", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ query: q }),
+//     });
+
+//     if (!res.ok) {
+//       const text = await res.text();
+//       console.error("API error:", text);
+//       setFoodResult(" API error. Check console.");
+//       return;
+//     }
+
+//     const data = await res.json();
+//     console.log("ES results:", data);
+//     setFoodResult(
+//       data ? `Found: ${JSON.stringify(data)}` : "No results found.",
+//     );
+//   };
+
+//   return (
+//     <div className="w-full flex flex-col items-center gap-6 py-10">
+//       <h1 className="text-4xl font-semibold tracking-tight text-center">
+//         Ctrl + F(ood)
+//       </h1>
+
+//       <div className="w-full max-w-3xl">
+//         <SearchBox onSearch={handleSearch} />
+//       </div>
+
+//       {foodResult ? <pre className="...">{foodResult}</pre> : null}
+//     </div>
+//   );
+// }
+
 import { SearchBox } from "../components/SearchBox";
+import * as React from "react";
+import { ThemeProvider } from "../components/theme-provider";
+import 'leaflet/dist/leaflet.css';
+import OverlayWrapper from "../components/OverlayWrapper";
+import MapWrapper from "../components/MapWrapper";
 
-export default function MainPage() {
-  const [foodResult, setFoodResult] = React.useState<string | null>(null);
-
-  const handleSearch = async (q: string) => {
-    const res = await fetch("http://localhost:8000/query", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query: q }),
-    });
-
-    if (!res.ok) {
-      const text = await res.text();
-      console.error("API error:", text);
-      setFoodResult(" API error. Check console.");
-      return;
-    }
-
-    const data = await res.json();
-    console.log("ES results:", data);
-    setFoodResult(
-      data ? `Found: ${JSON.stringify(data)}` : "No results found.",
-    );
-  };
+function App() {
 
   return (
-    <div className="w-full flex flex-col items-center gap-6 py-10">
-      <h1 className="text-4xl font-semibold tracking-tight text-center">
-        Ctrl + F(ood)
-      </h1>
+    <ThemeProvider>
+      <div className="relative h-screen w-screen">
 
-      <div className="w-full max-w-3xl">
-        <SearchBox onSearch={handleSearch} />
+        <MapWrapper></MapWrapper>
+        <OverlayWrapper></OverlayWrapper>
+
       </div>
-
-      {foodResult ? <pre className="...">{foodResult}</pre> : null}
-    </div>
+    </ThemeProvider>
   );
 }
+
+export default App;
